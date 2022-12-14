d4d9bb3903da8d9d0c71d2b43725ecead4713d95
Alisson Santana <alissonsantana16608@gmail.com>
2022-12-14 14:53:32 -0300

    Criando as rotas e controllers

---
+++ src/controllers/pageController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+
+export const home = (req: Request, res: Response) => {
+    res.send("home no crontroller!");
+    // res.render("pages/page")
+}
+
+export const dogs = (req: Request, res: Response) => {
+
+    // res.render("pages/page")
+}
+
+export const cats = (req: Request, res: Response) => {
+
+    // res.render("pages/page")
+}
+
+export const fisches = (req: Request, res: Response) => {
+
+    // res.render("pages/page")
+}
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mustache from "mustache-express";
 import path from "path";
+import mainRoutes from "./routes/index"
 
 dotenv.config();
 
@@ -13,6 +14,10 @@ server.engine("mustache", mustache());
 
 server.use(express.static(path.join(__dirname, "../public")));
 
-// Rotas
+server.use(mainRoutes);
+
+server.use((req, res) => {
+    res.send("Página não encontrada!");
+});
 
 server.listen(process.env.PORT);
