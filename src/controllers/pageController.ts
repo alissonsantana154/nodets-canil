ade66b85bb7fa94372be4d78a26d604e60a92cf6
Alisson Santana <alissonsantana16608@gmail.com>
2022-12-15 09:42:52 -0300

    separando as views

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,21 +1,21 @@
 import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
-    res.send("home no crontroller!");
-    // res.render("pages/page")
+    res.render("pages/page")
 }
 
 export const dogs = (req: Request, res: Response) => {
-
+    res.send("dogs");
     // res.render("pages/page")
 }
 
 export const cats = (req: Request, res: Response) => {
+    res.send("cats");
 
     // res.render("pages/page")
 }
 
-export const fisches = (req: Request, res: Response) => {
-
+export const fishes = (req: Request, res: Response) => {
+    res.send("fishes");
     // res.render("pages/page")
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,7 +7,7 @@ const router = Router();
 router.get("/", PageController.home);
 router.get("/dogs", PageController.dogs);
 router.get("/cats", PageController.cats);
-router.get("/fisches", PageController.fisches);
+router.get("/fishes", PageController.fishes);
 
 router.get("/search", SearchController.search);
 
