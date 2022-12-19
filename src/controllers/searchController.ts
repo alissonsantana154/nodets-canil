58bf8509f3c7cf80248dc693419c44d0f81542da
Alisson Santana <alissonsantana16608@gmail.com>
2022-12-19 10:33:43 -0300

    Fazendo a busca e o 404

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,6 +1,16 @@
 import { Request, Response } from "express";
 
-export const search = (req: Request, res: Response) => {
+import { Pet } from "../models/pet";
+import { createMenuObject } from "../helpers/createMenuObject";
 
-    // res.render("pages/search")
+export const search = (req: Request, res: Response) => {
+    let query: string = req.query.q as string;
+
+    let list = Pet.getFromName(query);
+
+    res.render("pages/page", {
+        menu: createMenuObject(""),
+        list,
+        query
+    });  
 }
